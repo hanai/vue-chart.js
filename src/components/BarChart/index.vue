e096e3cb8e14440c7344eb62bed39c6421352936
hanai <ihanai1991@gmail.com>
2016-03-27 23:20:28 +0800

    UPDATE: brchart: stacked

--- src/components/BarChart/index.vue
+++ src/components/BarChart/index.vue
@@ -43,6 +43,13 @@ export default {
     responsive: {
       type: Boolean,
       default: true
+    },
+    stacked: {
+      type: Array,
+      validator(value) {
+        return value.every(label => typeof label === 'string');
+      },
+      default() { return []; }
     }
   },
   computed: {
@@ -55,6 +62,18 @@ export default {
     chartOptions() {
       let options = {};
       options.responsive = this.responsive;
+      options.scales = {
+        xAxes: [],
+        yAxes: []
+      };
+
+      if (this.stacked.indexOf('x')) {
+        options.scales.xAxes.push({stacked: true});
+      }
+
+      if (this.stacked.indexOf('y')) {
+        options.scales.yAxes.push({stacked: true});
+      }
 
       return Object.assign({}, this.options, options);
     }
